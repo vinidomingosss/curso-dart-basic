8d3d8aff8c44b817ca51c4ae9bd23585446a6496
Vinicius Domingos <viniciusda95@gmail.com>
2019-10-18 16:03:03 -0300

    metodo construtor por {}

--- objetos/lib/main.dart
+++ objetos/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:objetos/src/pessoa.dart';
 
 main(List<String> arguments) {
-  Pessoa pessoa = Pessoa();
+  Pessoa pessoa = Pessoa(idade: 19,nome: 'Vinicius',sexo: 'M');
   print(pessoa.nome);
   print(pessoa.idade);
   print(pessoa.sexo);
--- objetos/lib/src/pessoa.dart
+++ objetos/lib/src/pessoa.dart
@@ -1,6 +1,10 @@
 class Pessoa {
-  
-  String nome = 'vinicius';
-  int idade = 19;
-  String sexo = 'M';
+  String nome;
+  int idade;
+  String sexo;
+
+  Pessoa({this.nome, this.idade, this.sexo});
+
+  //pode colocar as chaves, dai na hora de criar nao importa a ordem que passar os paramentros
+  //ou sem as chaves e na hora de criar ele da um hint da ordem
 }
